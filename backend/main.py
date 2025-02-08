d0cdf2d23a030e5af164befa25849f8c9b4b87a5
josh-wjp <joshua@wjp.ai>
2025-02-08 17:54:50 -0500

    Fixing Typos in main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 #//Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://hodlbot.wjp.ai"],  # Allow the frontend origin
+    allow_origins=["https://hodlbot.wjp.ai", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
